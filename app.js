676f13c10bd0931a21ff9e9e90a8d5c72a38a900
TECOA136_Aditya_ Deo <ahdeo2015@gmail.com>
2021-09-03 16:00:32 +0530

    add styles

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 var options = {
     weekday : "long",
